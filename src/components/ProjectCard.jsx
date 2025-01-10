203792cffd4a1eb5d18ae5d0bc26939a4c360fa8
Itzfeminisce <rofesol.ng@gmail.com>
2025-01-10 10:22:41 +0100

    chore: UI Refactor

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,17 +1,27 @@
-import Image from 'next/image';
-import styles from '../styles/ProjectCard.module.css';
+import Image from "next/image";
+import styles from "../styles/ProjectCard.module.css";
 
 const ProjectCard = ({ project }) => {
   return (
     <div className={styles.card}>
       <div className={styles.imgContainer}>
-      <Image src={project.image} height={300} width={600} alt={project.name} unoptimized={true}/>
+        <Image
+          src={project.image}
+          height={300}
+          width={600}
+          alt={project.name}
+          unoptimized={true}
+        />
       </div>
       <div className={styles.content}>
         <h3>{project.name}</h3>
         <p>{project.description}</p>
-        <h4 className={styles.role}>Role</h4>
-        <p>{project.role}</p>
+        {project.role && (
+          <>
+            <h4 className={styles.role}>Role</h4>
+            <p>{project.role}</p>
+          </>
+        )}
         <div className={styles.tags}>
           {project.tags.map((tag) => (
             <span key={tag} className={tag}>
